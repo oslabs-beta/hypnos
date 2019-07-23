3d978c62c54924f8a9d12af6adfd27f4fe79cfd9
Sophie <sophie.nye@gmail.com>
2019-07-23 17:02:41 -0400

    parcel working, react working, html working

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div >
+        )
+    }
+}
+
+
+export default App;
+
