0085c869d30daf91e9454fd44533cf1887d80475
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-07-24 16:48:45 -0400

    Working api endpoint field

--- client/Components/EndpointInput.jsx
+++ client/Components/EndpointInput.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStateValue } from '../Context';
 
 const EndpointInput = () => {
   // just uncomment when you want you start using Menu!
   const [{ endpoint, greeting }, dispatch] = useStateValue();
+  const [textValue, setTextValue] = useState();
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    console.log(textValue, 'this is textValue')
+    dispatch({
+      type: 'addEndpoint',
+      addEndpoint: textValue
+    })
+  }
 
 
   return (
     <div>
-      <button
-        onClick={() => dispatch({
-          type: 'addEndpoint',
-          addEndpoint: 'www.swapi.com'
-        })}
-      >
-        Change the endpoint!
-      </button>
+      {/* <form>
+        <input type='text' />
+        <input type='submit' value='submit' 
+          onSubmit={() => dispatch({
+            type: 'addEndpoint',
+            addEndpoint: event.target.value
+        })}/>
+      </form> */}
+      <form onSubmit={() => handleSubmit()}>
+        <textarea value={textValue} onChange={(e) => setTextValue(e.target.value)} />
+        <input type="submit" value="Submit" />
+      </form>
       <h3>
         {endpoint}
       </h3>
