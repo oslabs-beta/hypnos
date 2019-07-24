0f52a3f6e6512dc948d9d46c5c49aeb3ba93e61c
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-07-24 17:15:46 -0400

    refactored endpointinput

--- client/Components/EndpointInput.jsx
+++ client/Components/EndpointInput.jsx
@@ -3,31 +3,24 @@ import { useStateValue } from '../Context';
 
 const EndpointInput = () => {
   // just uncomment when you want you start using Menu!
-  const [{ endpoint, greeting }, dispatch] = useStateValue();
-  const [textValue, setTextValue] = useState();
+  const [{ endpoint, greeting, url }, dispatch] = useStateValue();
 
   const handleSubmit = () => {
     event.preventDefault();
-    console.log(textValue, 'this is textValue')
+    console.log(url, 'this is textValue')
     dispatch({
-      type: 'addEndpoint',
-      addEndpoint: textValue
+      type: 'submitEndpoint',
+      submitEndpoint: url
     })
   }
 
-
   return (
     <div>
-      {/* <form>
-        <input type='text' />
-        <input type='submit' value='submit' 
-          onSubmit={() => dispatch({
-            type: 'addEndpoint',
-            addEndpoint: event.target.value
-        })}/>
-      </form> */}
       <form onSubmit={() => handleSubmit()}>
-        <textarea value={textValue} onChange={(e) => setTextValue(e.target.value)} />
+        <textarea  onChange={(e) => dispatch({
+          type: 'addURL',
+          addURL: e.target.value
+        })} />
         <input type="submit" value="Submit" />
       </form>
       <h3>
