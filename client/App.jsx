38041f7f6945e89fa3bf25c70d4ed970c9b0c9ed
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-07-25 10:43:41 -0400

    Removed gql from app

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StateProvider } from './Context';
 import QueriesContainer from './Containers/QueriesContainer'
 import Menu from './Components/Menu'
-import gql from 'graphql-tag';
 
 
 const App = () => {
