fcb8ced5ac915b52c5710908c9dfb4390998919e
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-07-26 16:57:34 -0400

    error message for incorrect GQL data fields

--- client/Components/QueryOutputDisplay.jsx
+++ client/Components/QueryOutputDisplay.jsx
@@ -6,8 +6,9 @@ const QueryOutputDisplay = (props) => {
   const [{ endpoint, queryResultObject, queryResult404 }, dispatch] = useStateValue();
   // pull props off
   const { loading, error } = props;
-  const result = props[queryResultObject] ? props[queryResultObject] : '' ;
-  console.log('result in queryoutputdisplay ', result)
+  const result = props[queryResultObject] ? props[queryResultObject] : queryResult404 ;
+  // console.log('result in queryoutputdisplay ', result)
+  let testNull = Object.values(result).includes(null)
 
   // loading and error cases do not have query-output IDs
   if (loading) {
@@ -18,19 +19,18 @@ const QueryOutputDisplay = (props) => {
     return (<h4>{error.message}</h4>);
   }
 
+  if (testNull) return (<h4>Error in GQL types</h4>)
+
   return (
     <>
-      <h2>inside QueryOutput</h2>
       <h3>
         <pre>
           <code>
             {jsonFormatter(result)}
-            {/* {result} */}
           </code>
         </pre>
       </h3>
     </>
-
   );
 };
 
