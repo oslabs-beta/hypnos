386842e151b2250ef7fbb39e2ca5925a4cdf3bb5
Will Robinson <wrobinson91@gmail.com>
2019-07-31 16:37:15 -0400

    deleted 404 error type from constants

--- client/Constants/actionTypes.js
+++ client/Constants/actionTypes.js
@@ -2,5 +2,4 @@ export const RUN_QUERY = 'RUN_QUERY';
 export const ADD_URL = 'ADD_URL';
 export const SUBMIT_ENDPOINT = 'SUBMIT_ENDPOINT';
 export const RESET_STATE = 'RESET_STATE';
-export const ERROR_404 = 'ERROR_404';
 export const GQL_ERROR = 'GQL_ERROR';
