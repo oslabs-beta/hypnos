11e1b25b88ab8a4fd8e797bae826b1577a4990b8
Will Robinson <wrobinson91@gmail.com>
2019-07-30 22:40:00 -0400

    finally moved error handling into other file

--- client/Components/QueryInput.jsx
+++ client/Components/QueryInput.jsx
@@ -40,6 +40,24 @@ const QueryInput = () => {
     // console.log(textValue.match(/(?<=path:\W*\")\S*(?=\")/gi)[0].trim())
     // console.log('submitted to: ', urlToSend);
     // make initial fetch to api, to ensure endpoint is valid. proxy to get around CORS
+
+
+    // // ! THIS IS A TEST FOR NESTED QUERIES:
+    // dispatch({
+    //   type: types.RUN_QUERY,
+    //   // decontructed using of gql tag to make query object. need to pass in a stringliteral.
+    //   query: gql([`${textValue}`]),
+    //   // pulls of key for where data will be in result obj
+    //   queryResultObject: textValue.match(/(?<=\{\W)(.*?)(?=\@)/g)[0].trim(),
+    //   newEndpoint: urlToSend,
+    // });
+    // // reset local api endpoint
+    // setNewAPIEndpoint('');
+
+    // return;
+    // // ! END OF TEST FOR NESTED QUERIES
+
+
     fetch(proxy + urlToSend, {
       // mode: 'no-cors',
       headers: {
@@ -49,7 +67,7 @@ const QueryInput = () => {
     })
       .then((response) => {
         // catch all for when textValue is null
-        const pathRegex = textValue.match(/(?<=path:\W*\")\S*(?=\")/gi)
+        const pathRegex = textValue.match(/(?<=path:\W*\")\S*(?=\")/gi);
         if (response.status === 404) {
           // moved 404 check into first then, to actually check for status code
           dispatch({
@@ -69,8 +87,8 @@ const QueryInput = () => {
             headers: {
               'Content-Type': 'application/json',
             },
-          })
-        };
+          });
+        }
       })
       // for checking if the path is correct
       .then((response) => {
@@ -96,52 +114,8 @@ const QueryInput = () => {
         setNewAPIEndpoint('');
       })
       .catch((error) => {
-        // fetchErrorCheck(error, dispatch);
-        // // if Gql query does not start with 'query'
-        // return;
-
-        if (error.message.slice(0, 29) === 'Syntax Error: Unexpected Name') {
-          dispatch({
-            type: types.GQL_ERROR,
-            // line 1 is hardcoded in.
-            result404: 'Query method is invalid. Please double check your query on line 1.',
-          });
-          // if the variable before @rest does not exist
-          // ! TODO: this doesn't look like it's firing.
-        } else if (error.message === 'Syntax Error: Expected Name, found @') {
-          dispatch({
-            type: types.GQL_ERROR,
-            result404: 'Variable before "@rest" cannot be blank. Please click reset and check line 3 of the example for reference.',
-          });
-          // if query does not have proper curly brackets
-          // ! TODO: this didn't look like it fired for inner right bracket being closed. Or final right bracket
-        } else if (error.message === 'Syntax Error: Expected Name, found <EOF>' || error.message.slice(0, 24) === 'Syntax Error: Expected {' || error.message.slice(0, 26) === 'Syntax Error: Unexpected }') {
-          dispatch({
-            type: types.GQL_ERROR,
-            result404: 'Query must be wrapped in curly brackets.',
-          });
-          // if the query fields are blank
-          // ! TODO: this doesn't look like it's firing
-        } else if (error.message === 'Syntax Error: Expected Name, found }') {
-          dispatch({
-            type: types.GQL_ERROR,
-            result404: 'Query fields cannot be blank. Please click "Reset" and check line 4 of the example for reference.',
-          });
-        } else if (error.message.slice(0, 24) === 'Syntax Error: Expected :') {
-          dispatch({
-            type: types.GQL_ERROR,
-            result404: 'Inside @rest, "type" must be followed by a colon (e.g. type:).',
-          });
-          // ! TODO: this needs work. There are several errors that come through with the same error name and we'll have to figure out how best to parse them
-          // ! fires if string after "type" is empty
-        } else if (error.message.slice(0, 27) === 'Syntax Error: Expected Name') {
-          dispatch({
-            type: types.GQL_ERROR,
-            result404: '@rest must have a \'path\' and \'type\' property. Please click reset to check the example for reference.',
-          });
-        } else {
-          console.log('Error in fetch: ', error);
-        }
+        // moved error checking to other file for code clarity
+        fetchErrorCheck(error, dispatch);
       });
   };
 
--- client/utils/fetchErrorCheck.js
+++ client/utils/fetchErrorCheck.js
@@ -1,25 +1,12 @@
 import * as types from '../Constants/actionTypes';
 
 const fetchErrorCheck = (error, dispatch) => {
+  // if Gql query does not start with 'query'
   if (error.message.slice(0, 29) === 'Syntax Error: Unexpected Name') {
     dispatch({
       type: types.GQL_ERROR,
       // line 1 is hardcoded in.
       result404: 'Query method is invalid. Please double check your query on line 1.',
-    });
-    // ! TODO: this needs work. There are several errors that come through with the same error name and we'll have to figure out how best to parse them
-    // ! fires if string after "type" is empty
-  } else if (error.message.slice(0, 27) === 'Syntax Error: Expected Name') {
-    dispatch({
-      type: types.GQL_ERROR,
-      result404: '@rest must have a \'path\' and \'type\' property. Please click reset to check the example for reference.',
-    });
-    // if query does not have proper curly brackets
-    // ! TODO: this didn't look like it fired for inner right bracket being closed. Or final right bracket
-  } else if (error.message === 'Syntax Error: Expected Name, found <EOF>' || error.message.slice(0, 24) === 'Syntax Error: Expected {' || error.message.slice(0, 26) === 'Syntax Error: Unexpected }') {
-    dispatch({
-      type: types.GQL_ERROR,
-      result404: 'Query must be wrapped in curly brackets.',
     });
     // if the variable before @rest does not exist
     // ! TODO: this doesn't look like it's firing.
@@ -27,6 +14,13 @@ const fetchErrorCheck = (error, dispatch) => {
     dispatch({
       type: types.GQL_ERROR,
       result404: 'Variable before "@rest" cannot be blank. Please click reset and check line 3 of the example for reference.',
+    });
+    // if query does not have proper curly brackets
+    // ! TODO: this didn't look like it fired for inner right bracket being closed. Or final right bracket
+  } else if (error.message === 'Syntax Error: Expected Name, found <EOF>' || error.message.slice(0, 24) === 'Syntax Error: Expected {' || error.message.slice(0, 26) === 'Syntax Error: Unexpected }') {
+    dispatch({
+      type: types.GQL_ERROR,
+      result404: 'Query must be wrapped in curly brackets.',
     });
     // if the query fields are blank
     // ! TODO: this doesn't look like it's firing
@@ -39,6 +33,13 @@ const fetchErrorCheck = (error, dispatch) => {
     dispatch({
       type: types.GQL_ERROR,
       result404: 'Inside @rest, "type" must be followed by a colon (e.g. type:).',
+    });
+    // ! TODO: this needs work. There are several errors that come through with the same error name and we'll have to figure out how best to parse them
+    // ! fires if string after "type" is empty
+  } else if (error.message.slice(0, 27) === 'Syntax Error: Expected Name') {
+    dispatch({
+      type: types.GQL_ERROR,
+      result404: '@rest must have a \'path\' and \'type\' property. Please click reset to check the example for reference.',
     });
   } else {
     console.log('Error in fetch: ', error);
