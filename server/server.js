3dcd45f9ecfb938629f1ce2725c337f6306c428a
Will Robinson <wrobinson91@gmail.com>
2019-08-01 11:40:24 -0400

    server file further setup, testMethod in QI

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const bodyParser = require('body-parser');
 
-// localhost frontend is 3000
+// localhost is running on 3000
 const port = 3030;
 
-// more options here for cors package
+// more options here for cors package: https://www.npmjs.com/package/cors
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,11 +20,16 @@ app.use(bodyParser.json());
 //   next();
 // });
 
+// do we need a default route? not sure
+app.use('/static', express.static(path.join(__dirname, '../build')));
+app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../index.html')));
+
 app.get('/api', (req, res) => {
   console.log('at api endpoint');
-  return res.status(200).send('api queried');
+  return res.status(200).json({ msg: 'api queried' });
 });
 
+// 404 check. our server should only be making calls to '/api'
 app.get('*', (req, res) => {
   res.status(404).send('Path does not exist.');
 });
