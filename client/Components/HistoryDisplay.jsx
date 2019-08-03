89fa3bb9de4d42a4c5380db3142cdaac527f8d14
Sophie <sophie.nye@gmail.com>
2019-08-03 16:26:09 -0400

    added history tab, added dexie db, new queries added to dexie db and displayed on history display on being added
    Co-authored-by: Dillon Garrett <dillon.garrett.dev@gmail.com>
    Co-authored-by: Sophie Nye <sophie.nye@gmail.com>

---
+++ client/Components/HistoryDisplay.jsx
@@ -0,0 +1,42 @@
+import React, { Component, useEffect } from 'react';
+import db from '../db';
+import { useStateValue } from '../Context';
+import * as types from '../Constants/actionTypes';
+
+
+const HistoryDisplay = () => {
+    
+    const [{ queriesHistory }, dispatch] = useStateValue();
+
+    useEffect(() => {
+
+        db.history
+            .toArray()
+            .then((queries) => {
+                console.log('retrieved from DB', queries)
+                dispatch({
+                    type: types.UPDATE_HISTORY,
+                    queriesHistory: queries
+                });
+            })
+    }, [])
+
+
+    console.log(queriesHistory)
+    const historyList = queriesHistory.map(el => {
+        return <li id='el.id'>{el.query}</li>
+    })
+
+    console.log('historyList ', historyList)
+    return (
+        <div>
+            <ul id="history-display">
+                {historyList}
+            </ul>
+        </div>
+    )
+
+
+};
+
+export default HistoryDisplay;
