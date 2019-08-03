044288a1d1d279b5624351127320d8a38eac4399
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-08-03 17:05:59 -0400

    merge

--- client/Components/HistoryDisplay.jsx
+++ client/Components/HistoryDisplay.jsx
@@ -5,36 +5,33 @@ import * as types from '../Constants/actionTypes';
 
 
 const HistoryDisplay = () => {
-    
-    const [{ queriesHistory }, dispatch] = useStateValue();
-
-    useEffect(() => {
-
-        db.history
-            .toArray()
-            .then((queries) => {
-                console.log('retrieved from DB', queries)
-                dispatch({
-                    type: types.UPDATE_HISTORY,
-                    queriesHistory: queries
-                });
-            })
-    }, [])
+  const [{ queriesHistory }, dispatch] = useStateValue();
+  useEffect(() => {
+    db.history
+      .toArray()
+      .then((queries) => {
+        console.log('retrieved from DB', queries)
+        dispatch({
+          type: types.UPDATE_HISTORY,
+          queriesHistory: queries
+        });
+      })
+  }, [])
 
 
-    console.log(queriesHistory)
-    const historyList = queriesHistory.map(el => {
-        return <li id='el.id'>{el.query}</li>
-    })
+  console.log(queriesHistory)
+  const historyList = queriesHistory.map(el => {
+    return <li id='el.id'>{el.query}</li>
+  })
 
-    console.log('historyList ', historyList)
-    return (
-        <div>
-            <ul id="history-display">
-                {historyList}
-            </ul>
-        </div>
-    )
+  console.log('historyList ', historyList)
+  return (
+    <div>
+      <ul id="history-display">
+        {historyList}
+      </ul>
+    </div>
+  )
 
 
 };
