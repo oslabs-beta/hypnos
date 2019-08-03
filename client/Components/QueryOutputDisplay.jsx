fcfdb4d0275e2b295b0b8d46b513e3fb00069e52
Will Robinson <wrobinson91@gmail.com>
2019-08-03 14:48:18 -0400

    nullchecker and nullresult generator written. however it doesn't account if a value is ACTUALLY null

--- client/Components/QueryOutputDisplay.jsx
+++ client/Components/QueryOutputDisplay.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStateValue } from '../Context';
 import { jsonFormatter } from '../utils/jsonFormatter';
+import nullChecker from '../utils/nullChecker';
+import nullResultChecker from '../utils/nullResultChecker';
 
 const QueryOutputDisplay = (props) => {
   // ! TODO: MOVE ERROR CHECKING INTO A DIFFERENT FILE BECAUSE THIS IS A LOT
@@ -11,26 +13,27 @@ const QueryOutputDisplay = (props) => {
   const result = props[queryResultObject] ? props[queryResultObject] : queryGQLError;
 
   // checking if __typeName on the result object exists. If it doesn't, we send an error message
-  if (loading === false && !Object.keys(result).includes('__typename')) return <p className='error'>Query does not have a properly formatted type within @rest.</p>;
+  if (loading === false && !Object.keys(result).includes('__typename')) return <p className="error">Query does not have a properly formatted type within @rest.</p>;
 
   // checking to see if there are any null values on the results object
   // if so, means that the query field was improperly named or doesn't exist
 
   // ! NOTE: Only checks one level. need to do recursive check for nested nulls
-  const testNull = Object.values(result).includes(null);
+  const testNull = nullChecker(result);
   let nullVals;
   // if there is a null value in successful query, build LIs that display null props in query
   if (testNull) {
-    nullVals = Object.keys(result).reduce((acc, curVal) => {
-      if (result[curVal] === null) {
-        acc.push(
-          <li>
-            {curVal}
-          </li>,
-        );
-      }
-      return acc;
-    }, []);
+    nullVals = nullResultChecker(result);
+    // nullVals = Object.keys(result).reduce((acc, curVal) => {
+    //   if (result[curVal] === null) {
+    //     acc.push(
+    //       <li>
+    //         {curVal}
+    //       </li>,
+    //     );
+    //   }
+    //   return acc;
+    // }, []);
   }
 
   // checking if there are any values from our result that look like a url (surface level only)
@@ -75,17 +78,17 @@ const QueryOutputDisplay = (props) => {
   // any error from a graphql query that's not already accounted for is rendered here
   if (error) {
     if (error.message === 'Network error: forward is not a function') {
-      return (<p className='error'>Query submitted did not have '@rest' formatted correctly. For an example, press 'reset' and refer to line 3.</p>);
+      return (<p className="error">Query submitted did not have '@rest' formatted correctly. For an example, press 'reset' and refer to line 3.</p>);
     }
-    return (<p className='error'>{error.message}</p>);
+    return (<p className="error">{error.message}</p>);
   }
 
   // NOTE: If this is true, then successful query results will now be shown at all, which is OK.
   if (testNull) {
     return (
       <article>
-        <p font="helevtica" className='error'>
-Null values returned from query. Please check these properties:
+        <p font="helevtica" className="error">
+          Null values returned from query. Please check these properties:
           <br />
           <br />
         </p>
@@ -111,8 +114,8 @@ Null values returned from query. Please check these properties:
           {urlAsPropCheck
             ? (
               <article>
-                <p className='error'>
-Note: The following data on the prop(s) below resemble a URL. If it is, you will have to reformat your query to access data at that API:
+                <p className="error">
+                  Note: The following data on the prop(s) below resemble a URL. If it is, you will have to reformat your query to access data at that API:
                   <br />
                   <br />
                 </p>
