a35593139dfb4b39662162d63ffbba34eb9e96cb
Will Robinson <wrobinson91@gmail.com>
2019-08-06 12:03:20 -0400

    added error msg constants into fetchErrorCheck

--- client/utils/queryInput/fetchErrorCheck.js
+++ client/utils/queryInput/fetchErrorCheck.js
@@ -4,17 +4,18 @@
 */
 
 import * as types from '../../Constants/actionTypes';
+import * as errorMsg from '../../Constants/errors/errorStrings';
 
 const fetchErrorCheck = (error, dispatch, reject) => {
   // if Gql query does not start with 'query'
-  if (error.message.slice(0, 29) === 'Syntax Error: Unexpected Name') {
+  if (error.message.slice(0, errorMsg.queryMethodError.length) === errorMsg.queryMethodError) {
     dispatch({
       type: types.GQL_ERROR,
       // line 1 is hardcoded in.
       gqlError: 'Query method is invalid. Please double check your query on line 1.',
     });
     reject(new Error('Query method is invalid. Please double check your query on line 1.'));
-  } else if (error.message.slice(0, 12) === 'react-apollo') {
+  } else if (error.message.slice(0, errorMsg.multipleQueriesError.length) === errorMsg.multipleQueriesError) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: 'Currently attempting to run multiple queries, but only one query, subscription, or mutation may be run at one time.',
@@ -22,7 +23,7 @@ const fetchErrorCheck = (error, dispatch, reject) => {
     reject(new Error('Currently attempting to run multiple queries, but only one query, subscription, or mutation may be run at one time.'));
     // if the variable before @rest does not exist
     // ! TODO: this doesn't look like it's firing.
-  } else if (error.message === 'Syntax Error: Expected Name, found @') {
+  } else if (error.message === errorMsg.varBeforeRestError) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: 'Variable before "@rest" cannot be blank. Please click reset and check line 3 of the example for reference.',
@@ -30,7 +31,7 @@ const fetchErrorCheck = (error, dispatch, reject) => {
     reject(new Error('Variable before "@rest" cannot be blank. Please click reset and check line 3 of the example for reference.'));
     // if query does not have proper curly brackets
     // ! TODO: this didn't look like it fired for inner right bracket being closed. Or final right bracket
-  } else if (error.message === 'Syntax Error: Expected Name, found <EOF>' || error.message.slice(0, 24) === 'Syntax Error: Expected {' || error.message.slice(0, 26) === 'Syntax Error: Unexpected }') {
+  } else if (error.message === errorMsg.curlyBracketError1 || error.message.slice(0, errorMsg.curlyBracketError2.length) === errorMsg.curlyBracketError2 || error.message.slice(0, errorMsg.curlyBracketError3.length) === errorMsg.curlyBracketError3) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: 'Query must be wrapped in curly brackets.',
@@ -38,20 +39,20 @@ const fetchErrorCheck = (error, dispatch, reject) => {
     reject(new Error('Query must be wrapped in curly brackets.'));
     // if the query fields are blank
     // ! TODO: this doesn't look like it's firing
-  } else if (error.message === 'Syntax Error: Expected Name, found }') {
+  } else if (error.message === errorMsg.queryFieldBlankError) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: 'Query fields cannot be blank. Please click "Reset" and check line 4 of the example for reference.',
     });
     reject(new Error('Query fields cannot be blank. Please click "Reset" and check line 4 of the example for reference.'));
-  } else if (error.message.slice(0, 24) === 'Syntax Error: Expected :') {
+  } else if (error.message.slice(0, errorMsg.typeSyntaxError.length) === errorMsg.typeSyntaxError) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: 'Inside @rest, "type" must be followed by a colon (e.g. type:).',
     });
     reject(new Error('Inside @rest, "type" must be followed by a colon (e.g. type:).'));
     // TypeError: Cannot read property '0' of null
-  } else if (error.message === 'Cannot read property \'0\' of null') {
+  } else if (error.message === errorMsg.noRestCallError) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: 'Query must have an @rest call.',
@@ -59,7 +60,7 @@ const fetchErrorCheck = (error, dispatch, reject) => {
     reject(new Error('Query must have an @rest call.'));
     // ! TODO: this needs work. There are several errors that come through with the same error name and we'll have to figure out how best to parse them
     // ! fires if string after "type" is empty
-  } else if (error.message.slice(0, 27) === 'Syntax Error: Expected Name') {
+  } else if (error.message.slice(0, errorMsg.noPathOrTypeError.length) === errorMsg.noPathOrTypeError) {
     dispatch({
       type: types.GQL_ERROR,
       gqlError: '@rest must have a \'path\' and \'type\' property. Please click reset to check the example for reference.',
