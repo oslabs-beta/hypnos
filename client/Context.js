4bcec2a010cc7e5a400dd22bf556a74e840a624a
Will Robinson <wrobinson91@gmail.com>
2019-08-16 18:53:52 -0400

    tabs working truly? we think?
    Co-authored-by: Sophie Nye <sophie.nye@gmail.com>
    Co-authored-by: Dillon Garrett <dillon.garrett.dev@gmail.com>

--- client/Context.js
+++ client/Context.js
@@ -66,6 +66,7 @@ const reducer = (state, action) => {
     case types.RUN_QUERY:
       // when query is run, on button press, endpoint is assigned the dynamically changing url
       // console.log('run query fired, reducer', action);
+      console.log('headers: ', action.newHeadersKey, action.newAPIKey);
       return {
         ...state,
         // if a query is run, that means no 404 happened
